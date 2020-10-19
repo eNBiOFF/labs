c0b45cd99c44e4f2e51ae53f459d921e4238960f
Nikita Bakhramov <nikikostick@gmail.com>
2020-10-19 20:19:34 +0300

    Revert "Revert "Nikita""
    This reverts commit cf099947c8aa2a9ce1859b0ca586d12c2b7ab503.

---
+++ Vag-Fish/Vag-Fish/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vag_Fish
+{
+    public class VagFish
+    {
+        public static int Distant(string str1, string str2)
+        {
+            if ((str1 == null) || (str2 == null)) return -1;
+
+            int str1Len = str1.Length;
+            int str2Len = str2.Length;
+            if ((str1Len == 0)&&(str2Len == 0)) return 0;
+            if (str1Len == 0) return str2Len;
+            if (str2Len == 0) return str1Len;
+
+            string str1UP = str1.ToUpper();
+            string str2UP = str2.ToUpper();
+
+            int[,] matrix = new int[str1Len + 1, str2Len + 1];
+            for (int i = 0; i <= str1Len; i++) matrix[i, 0] = i;
+            for (int j = 0; j <= str2Len; j++) matrix[0, j] = j;
+
+            for(int i=0; i<=str1Len;i++)
+            {
+                for (int j=0;j<=str2Len;j++)
+                {
+                    int eq = ((str1UP.Substring(i - 1, j) == str2UP.Substring(1, j - 1)) ? 0 : 1);
+                    int isr = matrix[i, j - 1] + 1;
+                    int del = matrix[i - 1, j] + 1;
+                    int subsw = matrix[i - 1, j - 1] + eq;
+                    matrix[i, j] = Math.Min(Math.Min(isr, del), subsw);
+
+                    if ((i > 1) && (j > 1) &&
+                        (str1UP.Substring(i - 1, 1) == str2UP.Substring(j - 2, 1)) && 
+                        (str1UP.Substring(i - 2, 1) == str2UP.Substring(j - 1, 1)))
+                    {
+                        matrix[i, j] = Math.Min(matrix[i, j], matrix[i - 2, j - 2] + eq);
+                    }
+                }
+            }
+            return matrix[str1Len, str2Len];
+        }
+
+        public void WriteDistants(string param1, string param2)
+        {
+            int d = Distant(param1, param2);
+            System.Console.WriteLine(" ' " + param1 + "','" + param2 + "' ->" + d.ToString());
+        }
+        
+    }
+}
---
+++ lab05/lab05/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vag_Fish;
+using System.Diagnostics;
+using System.IO;
+
+namespace lab05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<string> wr = new List<string>();
+        public VagFish vag = new VagFish();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "*name file|*.txt";
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+
+                string text = File.ReadAllText(fd.FileName);
+                char[] separator = new char[] { ' ', '.', ',', '!', '?', '/', '\t', '\n' };
+                string[] arraytext = text.Split(separator);
+
+                foreach (string strtmp in arraytext)
+                {
+                    string str = strtmp.Trim();
+                    if (!wr.Contains(str)) wr.Add(str);
+                }
+
+               
+                this.textBox3.Text = fd.FileName.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string str = this.textBoxFindWr.Text.Trim();
+            string MaxDist = this.MaxDistant.Text.Trim();
+            if ((!string.IsNullOrEmpty(str))&&(!string.IsNullOrEmpty(MaxDist)))
+            {
+                string strUP = str.ToUpper();
+                int dist = int.Parse(MaxDist);
+                List<string> tmpstr = new List<string>();
+
+                foreach (string tmp in wr)
+                {
+                    string st = tmp.Trim();
+                    if (VagFish.Distant(st.ToUpper(), strUP) <= dist)
+                    {
+                        if (!tmpstr.Contains(st)) tmpstr.Add(st);
+                    }
+                }
+
+                this.listBoxFoundWR.BeginUpdate();
+                this.listBoxFoundWR.Items.Clear();
+                foreach(string tmp in tmpstr)
+                {
+                    this.listBoxFoundWR.Items.Add(tmp).ToString();
+                }
+                this.listBoxFoundWR.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("необходимо выбраь файл и ввести слово для поиска");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
